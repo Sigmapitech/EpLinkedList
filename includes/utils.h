2c1e8c555f7778e49b071da9ddcbaa570f213d49
Sigmanificient <edhyjox@gmail.com>
2022-11-25 14:13:45 +0100

    :label: Add utils header

---
+++ includes/utils.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** LinkedList - Mini LinkedList lib
+** File description:
+** Header file for utils definitions
+*/
+
+#ifndef SG_UTILS_
+    #define SG_UTILS_
+
+    #ifndef NULL
+        #define NULL ((void *)0)
+    #endif
+
+    #ifndef local
+        #define local static
+    #endif
+
+typedef unsigned int u_int;
+#endif
